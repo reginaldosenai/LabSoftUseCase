c922a3651721204f99053030b1373ab51d808c20
maria <maria.l.santos13@aluno.senai.br>
2026-08-05 16:34:07 -0300

    feat(usabilidade): adiciona marca da SoftwareHouse no titulo

--- SLN_Dias/AppDias/Program.cs
+++ SLN_Dias/AppDias/Program.cs
@@ -8,7 +8,7 @@ class Program
     {
         // Em Program.cs, modifique o bloco do cabeçalho:
         Console.WriteLine("=================================");
-        Console.WriteLine(" SISTEMA DE GESTÃO DE TAREFAS - Versão 1.0.0");
+        Console.WriteLine(" SoftwareHouse - SISTEMA DE GESTÃO DE TAREFAS");
         Console.WriteLine("=================================\n");
 
         Console.Write("Digite o nome da tarefa: ");
