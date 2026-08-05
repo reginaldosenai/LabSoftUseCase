6b63d04dd1d38aa74527b1c89668d5be7420134e
softsbuild <megaplusms2@gmail.com>
2026-08-05 10:48:43 -0300

    kk

--- SLN_Dias/AppDias/Program.cs
+++ SLN_Dias/AppDias/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("=================================");
-        Console.WriteLine("   SISTEMA DE GESTÃO DE TAREFAS 01");
+        Console.WriteLine("   SISTEMA DE GESTÃO DE TAREFAS 01 kk");
         Console.WriteLine("=================================\\n");
 
         Console.Write("Digite o nome da tarefa: ");
