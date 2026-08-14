78986829b69eb0e07b75304abff29825d31e29f2
dafukf <dl.aquino91@outlook.com>
2026-08-14 08:06:00 -0300

    Funcionário CRUD concluido

--- SLNTask/AppTask/Controllers/FuncionarioController.cs
+++ SLNTask/AppTask/Controllers/FuncionarioController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AppTask.Models;
 
-namespace AppTask.Views
+namespace AppTask.Controllers
 {
     public class FuncionarioController : Controller
     {
