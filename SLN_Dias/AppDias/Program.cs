450383e2b39928b3151429262751e02a8529e427
argollosz <165285524+argollosz@users.noreply.github.com>
2026-08-22 10:58:59 -0300

    feat(usabilidade):
     adiciona versao 1.0.0 no titulo

--- SLN_Dias/AppDias/Program.cs
+++ SLN_Dias/AppDias/Program.cs
@@ -6,8 +6,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Em Program.cs, modifique o bloco do cabeçalho:
         Console.WriteLine("=================================");
-        Console.WriteLine("   SISTEMA DE GESTÃO DE TAREFAS  ");
+        Console.WriteLine(" SISTEMA DE GESTÃO DE TAREFAS - Versão 1.0.0");
         Console.WriteLine("=================================\n");
 
         Console.Write("Digite o nome da tarefa: ");
